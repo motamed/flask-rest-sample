e1ec5567a15cde3d73048876ba1a91dd096872ae
Saeed Motamed <samotame@microsoft.com>
2019-02-03 11:06:08 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,68 @@
+# Flask REST Sample
+
+ This Repo Contains Simple REST API that gets __Airport__ and __Airline__ Details
+
+## Requirements
+
+- python 2.7+
+- flask
+- flask_restful
+
+## Installation
+
+```bash
+$ pip install -r requirements.txt
+```
+
+## Run
+
+```bash
+$ python application.py
+
+ * Serving Flask app "application" (lazy loading)
+ * Environment: production
+   WARNING: Do not use the development server in a production environment.
+   Use a production WSGI server instead.
+ * Debug mode: on
+ * Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)
+ * Restarting with stat
+ * Debugger is active!
+```
+
+## Test
+
+> goto --> localhost:5000/
+
+![Alt text](/assets/it_works.png "it Works")
+
+## Start Using
+
+### GET
+
+#### Airports
+
+- ```localhost:5000/airport/{IATA_code}```
+- ```localhost:5000/airportname/{IATA_code}```
+- ```localhost:5000/airportlocation/{IATA_code}```
+
+#### Airlines
+
+- ```localhost:5000/airline/{IATA_code}```
+- ```localhost:5000/airlinecode/{airline_ID}```
+
+### POST
+
+- Soon ... !!
+
+## Example
+
+### Airport
+
+![Alt text](/assets/airport_example.png "Airport Example")
+### Airline
+
+![Alt text](/assets/airline_example.png "Airline Example")
+    
+## Reference
+
+> __Airport__ and __Airline__ data from https://openflights.org/data.html
---
+++ application.py
@@ -0,0 +1,63 @@
+import csv
+from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class start(Resource):
+    def get(self):
+        return 'It Works'
+
+
+class GetAirportDetails(Resource):
+    def get(self, APcode):
+        csv_file = csv.reader(open('airports.csv', "rb"), delimiter=",")
+        for row in csv_file:
+            if APcode == row[4]:
+                return jsonify({'Airport ID': row[0], 'Name': row[1], 'City': row[2], 'Country': row[3], 'IATA': row[4], 'ICAO': row[5], 'Latitude': row[6], 'Longitude': row[7]})
+
+
+class GetAirportName(Resource):
+    def get(self, APcode):
+        csv_file = csv.reader(open('airports.csv', "rb"), delimiter=",")
+        for row in csv_file:
+            if APcode == row[4]:
+                return jsonify({'Airport': row[1]})
+
+
+class GetAirportLocation(Resource):
+    def get(self, APcode):
+        csv_file = csv.reader(open('airports.csv', "rb"), delimiter=",")
+        for row in csv_file:
+            if APcode == row[4]:
+                return jsonify({'Latitude': row[6], 'Longitude': row[7]})
+
+
+class GetAirlineDetails(Resource):
+    def get(self, IATA):
+        csv_file = csv.reader(open('airlines.csv', "rb"), delimiter=",")
+        for row in csv_file:
+            if IATA == row[3]:
+                return jsonify({'Airline ID': row[0], 'Name': row[1], 'Alias': row[2], 'IATA': row[3], 'ICAO': row[4], 'Callsign': row[5], 'Country': row[6], 'Active': row[7]})
+
+
+class GetAirlineIATA(Resource):
+    def get(self, AID):
+        csv_file = csv.reader(open('airlines.csv', "rb"), delimiter=",")
+        for row in csv_file:
+            if AID == row[0]:
+                return jsonify({'Name': row[1], 'IATA': row[3]})
+
+
+api.add_resource(GetAirportDetails, '/airport/<string:APcode>')
+api.add_resource(GetAirportName, '/airportname/<string:APcode>')
+api.add_resource(GetAirportLocation, '/airportlocation/<string:APcode>')
+api.add_resource(GetAirlineDetails, '/airline/<string:IATA>')
+api.add_resource(GetAirlineIATA, '/airlinecode/<string:AID>')
+api.add_resource(start, '/')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
